aad118ac05df4cdb095441b13bdd2b7c35c3398e
gulbasozan <gulbasozan@gmail.com>
2024-11-25 19:46:44 +0300

    Add custom window controls key mappings

--- lua/gulbasozan/core/maps.lua
+++ lua/gulbasozan/core/maps.lua
@@ -4,4 +4,11 @@ local keymap = vim.keymap
 
 keymap.set('n', "<leader>pv", "<CMD>Ex<CR>")
 keymap.set({'i', 'v'}, "jf", "<ESC>")
+keymap.set("n", "<leader>ws", "<CMD>wincmd s<CR>")
+keymap.set("n", "<leader>wv", "<CMD>wincmd v<CR>")
+keymap.set("n", "<leader>wj", "<CMD>wincmd j<CR>")
+keymap.set("n", "<leader>wk", "<CMD>wincmd k<CR>")
+keymap.set("n", "<leader>wh", "<CMD>wincmd h<CR>")
+keymap.set("n", "<leader>wl", "<CMD>wincmd l<CR>")
+keymap.set("n", "<leader>wq", "<CMD>wincmd q<CR>")
 
